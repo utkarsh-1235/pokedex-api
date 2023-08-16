3a8021d5c751aa4f6e49388ad4c2fd4f4bf9aa01
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-08-16 06:31:54 +0530

    first commit

---
+++ src/Components/Pokedex/Pokedex.jsx
@@ -0,0 +1,13 @@
+import Search from "../Search/Search";
+import "./Pokedex.css";
+
+function Pokedex(){
+    return (
+        <div className="pokedex-wrapper">
+        <h1 id="pokedex-height">Pokedex</h1>
+        <Search/>
+        </div>
+    )
+}
+
+export default Pokedex;
